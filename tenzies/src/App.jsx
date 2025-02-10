a8f718597237cd2978758215043a4836f917e747
can <firatcnur@gmail.com>
2025-02-10 00:02:39 +0000

    use index to get Die index from parent

--- tenzies/src/App.jsx
+++ tenzies/src/App.jsx
@@ -1,13 +1,25 @@
 import Die from './components/die.jsx'
+import React from 'react'
 
 export default function App() {
 
-    // list of die values
-    let dice = [1, 2, 3, 4, 5]
+    let [dice, setDice] = React.useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+
+    function handleRoll() {
+        console.log("rolling")
+
+        // dice random number between 1 and 6
+        setDice(dice.map((value) => Math.floor(Math.random() * 6) + 1))
+    }
+
+    function handleDieClick(index) {
+        console.log("clicked die", index, dice[index])
+    }
 
     return <main>
-            <div className="dieContainer">
-                {dice.map((value, index) => <Die key={index} value={value}></Die>)}
-            </div>
-           </main>
+        <div className="dieContainerRow">
+            {dice.map((value, index) => <Die key={index} id={index} value={value} handleDieClick={handleDieClick}></Die>)}
+        </div>
+        <button onClick={handleRoll}>Roll</button>
+    </main>
 }
--- tenzies/src/components/die.jsx
+++ tenzies/src/components/die.jsx
@@ -1,6 +1,14 @@
 
 
+import React from 'react'
+
+
 export default function Die(props) {
 
-    return <button className="die">{props.value}</button>
+
+    function onClick(e) {
+        props.handleDieClick(props.id)
+    }
+
+    return <button className={props.removed ? "die-disabled" : "die"} onClick={onClick}>{props.value}</button>
 }
