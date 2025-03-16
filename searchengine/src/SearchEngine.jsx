8507001636e42a03374cf001a08b72d1c1ae86ae
can <firatcnur@gmail.com>
2025-03-16 23:03:31 +0000

    added left section with items at the bottom

--- searchengine/src/SearchEngine.jsx
+++ searchengine/src/SearchEngine.jsx
@@ -17,24 +17,33 @@ const SearchEngine = () => {
 
     return (
         <div className="app">
-            <h1 className="search-form-header">Search through Pipeline Knowledgebase</h1>
-            <div className="search-engine">
-                <form onSubmit={handleSearch} className="search-form">
-                    <input
-                        type="text"
-                        value={query}
-                        onChange={(e) => setQuery(e.target.value)}
-                        placeholder="Search..."
-                        className="search-input"
-                    />
-                    <button type="submit" className="search-button"></button>
-                </form>
-                <ul className="search-results">
-                    {results.map((result, index) => (
-                        <li key={index} className="search-result-item">{result}</li>
-                    ))}
-                </ul>
+            <div className="quicklinks">
+                <p>1</p>
+                <p>2</p>
+                <p>3</p>
             </div>
+
+            <div className="main">
+                <h1 className="search-form-header">Search through CICD Knowledgebase</h1>
+                <div className="search-engine">
+                    <form onSubmit={handleSearch} className="search-form">
+                        <input
+                            type="text"
+                            value={query}
+                            onChange={(e) => setQuery(e.target.value)}
+                            placeholder="Search..."
+                            className="search-input"
+                        />
+                        <button type="submit" className="search-button"></button>
+                    </form>
+                    <ul className="search-results">
+                        {results.map((result, index) => (
+                            <li key={index} className="search-result-item">{result}</li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+
         </div>
     );
 };
